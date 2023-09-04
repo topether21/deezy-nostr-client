83deab7787ad907a4a26ee97c2ce37e8ae22655f
topether21 <topether21@gmail.com>
2023-09-04 11:31:43 -0600

    1.0.69

--- src/queues/nostr.ts
+++ src/queues/nostr.ts
@@ -7,25 +7,31 @@ import { NosftEvent, RawNostrEvent } from './../types';
 import { addOnSaleItem } from './../cache';
 import { getInscriptionData } from './shared';
 import axios from 'axios';
-import { print } from './../utils';
 
 export const nostrConfig = {
   name: `${config.prefix}Nostr Events`,
+};
+
+export const trackingConfig = {
+  name: `${config.prefix}Tracking Events`,
 };
 
 export const nostrQueue = new Queue(nostrConfig.name, {
   connection,
 });
 
+export const trackingQueue = new Queue(trackingConfig.name, {
+  connection,
+});
+
 const notifyTrackerService = async (event: RawNostrEvent) => {
   try {
-    print(event);
     const inscriptionIdTag = event.tags.find((tag) => tag[0] === 'i');
     const inscriptionId = inscriptionIdTag ? inscriptionIdTag[1] : null;
     const url = `${config.trackingWebsite}/inscription/${inscriptionId}/refresh`;
-    console.log('Sending HTTP POST request to', url);
-    const { data } = await axios.post(url);
-    console.log('Tracker service notified', data);
+    // console.log('Sending HTTP POST request to', url);
+    const { status } = await axios.post(url);
+    console.log('Tracking service notified -->', status);
   } catch (error) {
     console.error('Error sending HTTP POST request', (error as Error).message);
   }
@@ -69,11 +75,27 @@ export const addOnSaleWorker = new Worker(
   }
 );
 
+export const trackingWorker = new Worker(
+  trackingQueue.name,
+  async ({ data }: Job<RawNostrEvent>) => {
+    await notifyTrackerService(data);
+    return {
+      status: 'ok',
+    };
+  },
+  {
+    concurrency: 10,
+    connection,
+  }
+);
+
 export const nostrWorker = new Worker(
   nostrQueue.name,
   async ({ data }: Job<RawNostrEvent>) => {
-    await notifyTrackerService(data);
-
+    await trackingQueue.add(trackingWorker.name, data, {
+      removeOnComplete: true,
+      removeOnFail: true,
+    });
     const inscription = await buildInscription(data);
 
     if (!inscription) return;
